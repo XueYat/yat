39fd4638a5155bb7d46ea0ba2ff4591b30c1d95c
XueYat <XueYat@gmail.com>
2021-12-10 16:25:35 +0800

    webSocket

--- websocket/src/main/java/top/yat/websocket/WebsocketApplication.java
+++ websocket/src/main/java/top/yat/websocket/WebsocketApplication.java
@@ -1,13 +1,25 @@
 package top.yat.websocket;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import top.yat.websocket.BulletChat.BulletChatServer;
+
+import javax.annotation.PostConstruct;
 
 @SpringBootApplication
 public class WebsocketApplication {
 
+    @Autowired
+    private BulletChatServer bulletChatServer;
+
     public static void main(String[] args) {
         SpringApplication.run(WebsocketApplication.class, args);
+    }
+
+    @PostConstruct
+    public void init(){
+        bulletChatServer.BulletChatServer();
     }
 
 }

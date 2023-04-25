aef706ad3b8052b6866146ca7ad893921b17ee24
XueYat <XueYat@gmail.com>
2023-04-25 09:30:47 +0800

    优化依赖关系

--- cpuHigh/src/main/java/top/yat/cpuhigh/controller/TestController.java
+++ cpuHigh/src/main/java/top/yat/cpuhigh/controller/TestController.java
@@ -1,6 +1,7 @@
 package top.yat.cpuhigh.controller;
 
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import top.yat.common.base.Result;
 import top.yat.common.utils.ResultUtil;
@@ -9,13 +10,21 @@ import top.yat.common.utils.ResultUtil;
  * @author XueYat
  * @date 2023/04/24
  */
-@RestController("/test")
+@RestController
+@RequestMapping("/test")
 public class TestController {
 
     @PostMapping("/hello")
     public Result hello() {
         String str = "hello";
         return ResultUtil.success(str);
+    }
+
+    @PostMapping("/cpu")
+    public Result cpu() {
+        while (true) {
+
+        }
     }
 
 }

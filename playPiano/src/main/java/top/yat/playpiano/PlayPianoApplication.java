5b3b1710671a0fa6e6d36fce2566f1062d376236
XueYat <XueYat@gmail.com>
2023-07-24 10:33:55 +0800

    piano

---
+++ playPiano/src/main/java/top/yat/playpiano/PlayPianoApplication.java
@@ -0,0 +1,13 @@
+package top.yat.playpiano;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlayPianoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PlayPianoApplication.class, args);
+    }
+
+}
---
+++ playPiano/src/test/java/top/yat/playpiano/PlayPianoApplicationTests.java
@@ -0,0 +1,13 @@
+package top.yat.playpiano;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlayPianoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
